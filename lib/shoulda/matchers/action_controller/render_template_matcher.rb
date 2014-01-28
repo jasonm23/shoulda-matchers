473cf0799e1f8dd7bd312cecd8950e65f6ad3a86
Elliot Winkler <elliot.winkler@gmail.com>
2014-01-28 13:09:22 -0700

    Add docs for render_template

--- lib/shoulda/matchers/action_controller/render_template_matcher.rb
+++ lib/shoulda/matchers/action_controller/render_template_matcher.rb
@@ -1,24 +1,41 @@
 module Shoulda
   module Matchers
     module ActionController
-      # Ensures a controller rendered the given template.
+      # The `render_template` matcher tests that an action renders a template.
+      # In RSpec, it is very similar to rspec-rails's `render_template`
+      # matcher. In Test::Unit, it provides a more expressive syntax over
+      # `assert_template`.
       #
-      # Example:
+      #     class PostsController < ApplicationController
+      #       def show
+      #       end
+      #     end
       #
-      #   it { should render_template(:show)  }
+      #     # RSpec
+      #     describe PostsController do
+      #       describe 'GET #show' do
+      #         before { get :show }
       #
-      #   assert that the "_customer" partial was rendered
-      #   it { should render_template(partial: '_customer')  }
+      #         it { should render_template('show') }
+      #       end
+      #     end
       #
-      #   assert that the "_customer" partial was rendered twice
-      #   it { should render_template(partial: '_customer', count: 2)  }
+      #     # Test::Unit
+      #     class PostsControllerTest < ActionController::TestCase
+      #       context 'GET #show' do
+      #         setup { get :show }
       #
-      #   assert that no partials were rendered
-      #   it { should render_template(partial: false)  }
+      #         should render_template('show')
+      #       end
+      #     end
+      #
+      # @return [RenderTemplateMatcher]
+      #
       def render_template(options = {}, message = nil)
         RenderTemplateMatcher.new(options, message, self)
       end
 
+      # @private
       class RenderTemplateMatcher
         attr_reader :failure_message, :failure_message_when_negated
 
