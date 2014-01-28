dd9edb0fa29d3264baf55afb9646a5a655ad134f
Elliot Winkler <elliot.winkler@gmail.com>
2014-01-28 13:51:57 -0700

    Remove module docs for ActionController, ActiveModel, ActiveRecord

--- lib/shoulda/matchers/active_record.rb
+++ lib/shoulda/matchers/active_record.rb
@@ -15,18 +15,6 @@ require 'shoulda/matchers/active_record/accept_nested_attributes_for_matcher'
 
 module Shoulda
   module Matchers
-    # = Matchers for your active record models
-    #
-    # These matchers will test the associations for your
-    # ActiveRecord models.
-    #
-    #   describe User do
-    #     it { should have_one(:profile) }
-    #     it { should have_many(:dogs) }
-    #     it { should have_many(:messes).through(:dogs) }
-    #     it { should belong_to(:lover) }
-    #   end
-    #
     module ActiveRecord
     end
   end
